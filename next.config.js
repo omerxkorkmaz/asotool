6eaf4066d3fa5df2dcf3bcdc9b07c4779d285a81
Omer Korkmaz <omerxkorkmaz@gmail.com>
2026-06-30 22:56:33 +0300

    bug fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: [
+      'google-play-scraper',
+      'app-store-scraper',
+      'googleapis',
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push(
+        'google-play-scraper',
+        'app-store-scraper',
+        'googleapis'
+      )
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
